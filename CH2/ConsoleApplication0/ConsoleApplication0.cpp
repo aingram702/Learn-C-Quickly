c9bfc21c8079ef0ed3f9af7be44554b8ae038bdc
Andrew <aingram702@outlook.com>
2021-09-05 09:46:05 -0400

    Add files via upload

---
+++ CH2/ConsoleApplication0/ConsoleApplication0.cpp
@@ -0,0 +1,19 @@
+// ConsoleApplication0.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// #include "pch.h"
+#include <iostream>
+
+// single line comment
+
+/* 
+multi
+line
+comment
+*/
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ CH3/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int age{}; // aghe is a variable of integer type
+    std::cout << "How old are you?" << std::endl;
+    std::cin >> age;
+    std::cout << "Wow, we are the same age! I am also " << age << " years old! HowBoutDat! << std::endl";
+
+}
